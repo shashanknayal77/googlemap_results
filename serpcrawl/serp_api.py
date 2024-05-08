c730baf0df0b33a01e0bfed57594997243cdb787
shashanknayal77 <134627759+shashanknayal77@users.noreply.github.com>
2024-05-08 11:59:14 +0530

    Add files via upload

---
+++ serpcrawl/serp_api.py
@@ -0,0 +1,61 @@
+import streamlit as st
+import geocoder
+import serpapi
+import pandas as pd
+
+# Get the user's location based on IP address
+g = geocoder.ip('me')
+st.set_page_config(layout="wide")
+if g.ok:
+    latitude, longitude = g.latlng
+    # st.write('Your current location:')
+    st.write('Latitude:', latitude)
+    st.write('Longitude:', longitude)
+else:
+    st.write('Unable to retrieve your location.')
+
+st.title('Google Maps Search Results')
+
+# Input fields
+api_key = 'e4c319a9ebe06f54d3e99b82ec7605428a0b713b5b63f1167b2a992c966269cc'
+type_search = st.text_input('Enter the search query:')       
+lang_lat = f"@{latitude},{longitude},13z"
+
+# Create SERP API client
+client = serpapi.Client(api_key=api_key)
+
+if st.button('Fetch Results'):
+    data = []
+    page_number = 0
+    index = 1
+    while True:
+        results = client.search({
+            'engine': 'google_maps',
+            'type': 'search',
+            'q': type_search,
+            'll': lang_lat,
+            'start': page_number * 20
+        })
+        local_results = results.get("local_results", [])
+        if not local_results:
+            break
+        for result in local_results:
+            result['position'] = index
+            data.append(result)
+            index += 1
+        page_number += 1
+
+    df = pd.DataFrame(data)
+    columns_to_keep = ['title', 'rating', 'reviews', 'unclaimed_listing', 'type', 'address', 'phone', 'website']
+    df = df[columns_to_keep]
+    # Display the results
+    st.write(df)
+
+    # Download button
+    csv = df.to_csv(index=False)
+    st.download_button(
+        label="Download CSV",
+        data=csv,
+        file_name=f"{type_search}.csv",
+        mime='text/csv'
+    )
